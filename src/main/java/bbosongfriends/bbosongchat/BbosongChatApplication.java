36bb8733addeb61fd863039af974134b3399fd60
jhseob <jhseob06@gmail.com>
2024-10-09 14:12:51 +0900

    FIRST COMMIT

---
+++ src/main/java/bbosongfriends/bbosongchat/BbosongChatApplication.java
@@ -0,0 +1,13 @@
+package bbosongfriends.bbosongchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbosongChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbosongChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bbosongfriends/bbosongchat/BbosongChatApplicationTests.java
@@ -0,0 +1,13 @@
+package bbosongfriends.bbosongchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbosongChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
